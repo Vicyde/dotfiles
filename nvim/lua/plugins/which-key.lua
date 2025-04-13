14cba93eff7c3ca12e6ab92652160003f49719e1
= <jreuhman@fastmail.nl>
2025-04-14 00:04:19 +0200

    Which-keys mappings adjusted to new specs for V3

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,21 +1,14 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  config  = function() 
+  config = function()
     local wk = require('which-key')
-
-    wk.register({ 
-      ["<leader>"] = {
-        e = { name="Explorer" },
-        s = { name="Split" },
-        t = { name="Tab" },
-        f = { name="Fuzzy" },
-      }
+    wk.add({
+      { "<leader>e", name="Explorer" },
+      { "<leader>s", name="Split" },
+      { "<leader>t", name="Tab" },
+      { "<leader>f", name="Fuzzy" },
     })
-  end,
-  init = function() 
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
   end,
   opts = {
 
