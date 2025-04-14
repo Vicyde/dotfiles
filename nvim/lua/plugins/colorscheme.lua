879b2d6c4604b765b3bd0df333a9b205264c44b7
= <jreuhman@fastmail.nl>
2025-04-14 18:59:44 +0200

    Few tweaks for the everforest-theme configuration

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,9 @@ return {
   priority=1000,
   lazy=false,
   config = function()
+    vim.opt.background = 'dark' 
     vim.g.everforest_enable_italic = true
+    vim.g.everforest_background = 'hard'
     vim.cmd("colorscheme everforest")
   end,
 }
