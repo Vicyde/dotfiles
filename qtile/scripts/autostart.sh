4d05a58d69793d28de8162a325c7bd15e65eb242
= <jreuhman@fastmail.nl>
2025-05-21 19:08:16 +0200

    Adding check for X11/Wayland and starts kdewallet

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-picom -bc
+feh --bg-scale /home/auc/Wallpapers/wp3272764--wallpapers.jpg
+
+if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+  picom -bc
+fi
+
+/usr/lib/pam_kwallet_init
+
 emacs --daemon
